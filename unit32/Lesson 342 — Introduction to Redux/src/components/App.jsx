374db073c16502b73e5de636478414ec36915f02
Michael <mikemikhailov2003@gmail.com>
2020-02-23 13:33:38 +0100

    Made form a controlled component

--- unit32/Lesson 342 — Introduction to Redux/src/components/App.jsx
+++ unit32/Lesson 342 — Introduction to Redux/src/components/App.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 
-const App = () => (
-  <div className="container">
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodoText: '',
+    }
+    this.handleInput = this.handleInput.bind(this);
+  }
 
-export default App;
+  handleInput(text) {
+    this.setState({ newTodoText: text});
+  }
+
+  render() {
+    const {newTodoText} = this.state;
+    return (
+      <div className="container">
+        <AddTodo handleInput={this.handleInput} newTodoText={newTodoText} />
+        <VisibleTodoList />
+        <Footer />
+      </div>
+    )
+  }
+}
--- unit32/Lesson 342 — Introduction to Redux/src/containers/AddTodo.jsx
+++ unit32/Lesson 342 — Introduction to Redux/src/containers/AddTodo.jsx
@@ -1,30 +1,35 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addTodo } from '../actions';
 
-const AddTodo = ({ dispatch }) => {
-  let input;
+const AddTodo = ({ handleInput, handleSubmit, newTodoText }) => {
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (!input.value.trim()) {
-            return;
-          }
-          dispatch(addTodo(input.value));
-          input.value = '';
-        }}
-      >
-        <input
-          ref={(node) => {
-            input = node;
-          }}
-        />
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => handleInput(e.target.value)} value={newTodoText} />
         <button type="submit">Add Todo</button>
       </form>
     </div>
   );
 };
-export default connect()(AddTodo);
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    handleSubmit: (e) => {
+      const { newTodoText, handleInput } = ownProps;
+      e.preventDefault();
+      if (newTodoText.trim()) {
+        dispatch(addTodo(ownProps.newTodoText));
+        handleInput('');
+      }
+    },
+  };
+};
+
+AddTodo.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleInput: PropTypes.func.isRequired,
+  newTodoText: PropTypes.string.isRequired,
+};
+export default connect(null, mapDispatchToProps)(AddTodo);
